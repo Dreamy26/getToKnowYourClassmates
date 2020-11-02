995a69f6bcbf19f756e00e198c30a3a20f4843bb
AWilson <dreamy26@icloud.com>
2020-11-02 17:42:08 -0500

    first commit

---
+++ getToKnow/getToKnow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace getToKnow
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
