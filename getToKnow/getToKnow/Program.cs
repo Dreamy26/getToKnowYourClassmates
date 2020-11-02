964bf4352add66b4d64dd4caad137e8572d15eba
AWilson <dreamy26@icloud.com>
2020-11-02 18:12:35 -0500

    adds classmates names and numbers

--- getToKnow/getToKnow/Program.cs
+++ getToKnow/getToKnow/Program.cs
@@ -7,6 +7,10 @@ namespace getToKnow
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our C# class. \nWhich student would you like to learn more about? \n(enter a number 1-20): ");
+
+            string[] classMatesNames = new string[] { "Tommy", "Tamesha", "Borrow", "Katie", "Mike", "Missy" };
+
+            int[] otherNumbers = new int[] { 0, 1, 2, 3, 4 };
         }
     }
 }
